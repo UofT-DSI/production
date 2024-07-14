09a4cd8f521d1efd4c319bc29187f2d2966df2d7
Gordon Geringas <geringas@gordons-laptop.home>
2024-07-13 21:29:36 -0400

    answered assignment_1

---
+++ 05_src/.ipynb_checkpoints/logger-checkpoint.py
@@ -0,0 +1,32 @@
+import logging
+from datetime import datetime
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+LOG_DIR = os.getenv('LOG_DIR', './logs/')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+
+def get_logger(name, log_dir = LOG_DIR, log_level = LOG_LEVEL):
+
+    '''
+    Set up a logger with the given name and log level.
+    '''
+    _logs = logging.getLogger(name)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    
+    f_handler = logging.FileHandler(os.path.join(log_dir, f'{ datetime.now().strftime("%Y%m%d_%H%M%S") }.log'))
+    f_format = logging.Formatter('%(asctime)s, %(name)s, %(filename)s, %(lineno)d, %(funcName)s, %(levelname)s, %(message)s')
+    f_handler.setFormatter(f_format)
+    _logs.addHandler(f_handler)
+    
+    s_handler = logging.StreamHandler()
+    s_format = logging.Formatter('%(asctime)s, %(filename)s, %(lineno)d, %(levelname)s, %(message)s')
+    s_handler.setFormatter(s_format)
+    _logs.addHandler(s_handler)
+    
+    _logs.setLevel(log_level)
+    return _logs
